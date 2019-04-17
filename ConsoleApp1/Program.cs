be68a9a4d88b1a17a0f0efb05836ea3b1abf02a6
visualstudio <visualstudio@122-PC09>
2019-04-17 12:36:53 -0400

    DD

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,36 @@ namespace ConsoleApp1
 {
     class Program
     {
+        string a;
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("The longest word is: ");
+            a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procession by an Analytical Engine");
+        }
+
+        public void MyFunctionA(string output)
+        {
+            string[] words = output.Split(' ');
+            string b;
+
+            int wordArrayLength = words.Length;
+            int x = 0;
+            foreach (var word in words)
+            {
+
+                foreach (var word2 in words)
+                {
+                    if (word2.Length > word.Length)
+                    {
+                        a = word2;
+
+                    }
+                }
+            }
+            Console.WriteLine("Longest word is " + a);
+            Console.ReadLine();
+
         }
     }
 }
